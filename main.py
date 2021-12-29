bf4451c849a0007430a1156a15c6ce3a2b676133
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2021-12-29 23:21:49 +0100

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import PyPDF2
+
+user = input('Enter the name of the PDF file:\n')
+book = open(user, 'rb')
+pdf_reader = PyPDF2.PdfFileReader(book)
+num_pages = pdf_reader.numPages
+
+play = pyttsx3.init()
+print('Playing audio book')
+
+for num in range(0, num_pages):
+    page = pdf_reader.getPage(num)
+
+    data = page.extractText()
+
+    play.say(data)
+
+    play.runAndWait()
