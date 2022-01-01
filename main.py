0e9f4218512bac7a55df51c823420b10f396ba98
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-01 04:50:12 +0100

    commit

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pyttsx3
 import PyPDF2
 
-user = input('Enter the name of the PDF file:\n')
-book = open(user, 'rb')
-pdf_reader = PyPDF2.PdfFileReader(book)
+try:
+    user = input('Enter the name of the PDF file:\n')
+    if len(user) == 0:
+        user = 'django.pdf'      
+    book = open(user, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(book)
+except FileNotFoundError:
+    print('File not found')
+    exit()
 num_pages = pdf_reader.numPages
 
 play = pyttsx3.init()
